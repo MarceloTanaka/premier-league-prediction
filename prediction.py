ed403f092f18ea7bfceeaf34c636d4ebac09fc75
MarceloTanaka <marcetanakasato@gmail.com>
2026-04-08 13:06:30 -0500

    normalized head_to_head games and applied a weight to it

--- prediction.py
+++ prediction.py
@@ -1,11 +1,10 @@
 # Statistics to use: 
-# xG(30%), xGA(25%), direct matchups(10%), current form (last 5 games)(15%), home/away strength(20%)
-
+# xG(30%), xGA(30%), last 6 head-to-head(5%), current form (last 5 games)(15%), home/away strength(20%)
 
 xG = 1.77
 xGA = 0.99
-
 results = "WDLDWLDLDWLWDDWWDLWWLWLLDWWDWDWWWWDWDW"
+head_to_head = [True, True, True, True, True, True]
 
 # Functions to normalize all the relevant metrics
 def normalized_current_form(results):
@@ -27,9 +26,25 @@ def normalized_xG(xG):
 def normalized_xGA(xGA):
     return (2-xGA)/2
 
+def normalized_head_to_head(head_to_head):
+    history = 0
+    for winner in head_to_head:
+        if winner == True:
+            history += 3
+        elif winner == False:
+            history += 0
+        else:
+            history += 1
+
+    max_points = len(head_to_head) * 3
+    return history/max_points
+
+
+# Applying weight to each normalized metric
 weighted_xG = normalized_xG(xG)*0.30 # 30%
-weighted_current_form = normalized_current_form(results)*0.15 #15%
-weighted_xGA = normalized_xGA(xGA)*0.25 # 25%
+weighted_current_form = normalized_current_form(results)*0.15 # 15%
+weighted_xGA = normalized_xGA(xGA)*0.30 # 30%
+weighted_head_to_head = normalized_head_to_head(head_to_head)*0.05 # 5%
 
 print(f"xG: {weighted_xG}")
 print(f"Current form: {weighted_current_form}")
